c9bca471868b2590cbe01128aa5898de6df60aeb
Teona46 <teonika2406@gmail.com>
2024-10-20 20:55:12 +0300

    Homework-12/10

--- src/Bookshelf/Book.java
+++ src/Bookshelf/Book.java
@@ -1,4 +1,38 @@
 package Bookshelf;
 
 public class Book {
+    private String bookName;
+    private Author author;
+    private Integer yearOfPublication;
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Integer getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(Integer yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Book: '" + bookName + "', " + author +
+                        ", year of publication: " + yearOfPublication;
+    }
 }
--- src/Bookshelf/Main.java
+++ src/Bookshelf/Main.java
@@ -1,4 +1,28 @@
 package Bookshelf;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Author author1 = new Author();
+        author1.setFirstName("Mike");
+        author1.setLastName("Omer");
+
+        Author author2 = new Author();
+        author2.setFirstName("Paulo");
+        author2.setLastName("Coelho");
+
+        Book book1 = new Book();
+        book1.setBookName("Inside the killer");
+        book1.setYearOfPublication(2018);
+        book1.setAuthor(author1);
+
+        Book book2 = new Book();
+        book2.setBookName("Alchemist");
+        book2.setYearOfPublication(1998);
+        book2.setAuthor(author2);
+
+
+        System.out.println(book1.toString());
+        System.out.println(book2.toString());
+    }
 }
