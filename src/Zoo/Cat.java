d62fbffd4b510ad5ce0edaa8bf4f6be10f1a5c46
Teona46 <teonika2406@gmail.com>
2024-11-07 19:27:26 +0300

    Homework-19/10

--- src/Zoo/Cat.java
+++ src/Zoo/Cat.java
@@ -1,8 +1,17 @@
 package Zoo;
 
 public class Cat extends Animal {
-    public Cat(String name) {
+    protected Cat(String name) {
         super(name);
+    }
 
+    @Override
+    public void makeSound() {
+        System.out.println(super.toString() + " издает звук: Мяу");
+    }
+
+    @Override
+    public void eatSomething() {
+        System.out.println(super.toString() + " ест рыбку");
     }
 }
