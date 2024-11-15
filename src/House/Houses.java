2451991e76a960bb2620826b7c0ca619a89c2779
Teona46 <teonika2406@gmail.com>
2024-11-15 19:19:03 +0300

    Homework-27/10

--- src/House/Houses.java
+++ src/House/Houses.java
@@ -1,4 +1,52 @@
 package House;
 
 public class Houses {
+    private int floors;
+    private int rooms;
+    private boolean isGarage;
+
+    @Override
+    public String toString() {
+        return "Houses{" +
+                "floors=" + floors +
+                ", rooms=" + rooms +
+                ", isGarage=" + isGarage +
+                '}';
+    }
+
+    public Houses(Builder builder) {
+        this.floors = builder.floors;
+        this.rooms = builder.rooms;
+        this.isGarage = builder.isGarage;
+    }
+
+    public static class Builder {
+        private int floors;
+        private int rooms;
+        private boolean isGarage;
+
+        public Builder() {
+        }
+
+        public Builder floors(int floors) {
+            this.floors = floors;
+            return this;
+        }
+
+        public Builder rooms(int rooms) {
+            this.rooms = rooms;
+            return this;
+        }
+
+        public Builder isGarage(boolean isGarage) {
+            this.isGarage = isGarage;
+            return this;
+
+        }
+
+        public Houses build() {
+            return new Houses(this);
+        }
+    }
+
 }
--- src/House/Main.java
+++ src/House/Main.java
@@ -3,5 +3,20 @@ package House;
 public class Main {
     public static void main(String[] args) {
 
+        Houses house1 = new Houses.Builder()
+                .floors(3)
+                .rooms(4)
+                .isGarage(true)
+                .build();
+        System.out.println(house1);
+
+        Houses house2 = new Houses.Builder()
+                .floors(2)
+                .rooms(5)
+                .isGarage(false)
+                .build();
+        System.out.println(house2);
+
+
     }
 }
